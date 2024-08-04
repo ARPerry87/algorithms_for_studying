f6472d4e88c11db98063bca9409b823bd913c320
Your Name <hydras15@gmail.com>
2024-08-04 17:36:42 -0500

    adding solution without needing to open array class

--- Projects/algorithms_for_studying/merge_sort.rb
+++ Projects/algorithms_for_studying/merge_sort.rb
@@ -32,3 +32,27 @@ class Array
     sorted + left + right
   end
 end
+
+#if not watning to do do array class
+
+def merge_sort(arr)
+  if arr.length <= 1 
+    arr
+  else
+    mid = (arr.length / 2).floor #so ruby doesn't round
+    left = merge_sort(arr[0..mid - 1])
+    right = merge_sort(arr[mid..arr.length])
+    merge(left, right)
+end
+
+def merge(left, right)
+  if left.empty?
+    right
+  elsif right.empty?
+    left
+  elsif left.first < right.first
+    [left.first] + merge(left[1..left.length], right)
+  else right.first < left.first
+    [right.first] + merge(left, right[1..right.length])
+  end
+end
