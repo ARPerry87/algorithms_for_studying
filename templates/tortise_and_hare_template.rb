cff54e6bbf663d4f1389ce14c681378f88687437
Your Name <hydras15@gmail.com>
2024-11-06 13:12:59 -0600

    better example of tortoise and hare algo

--- templates/tortise_and_hare_template.rb
+++ templates/tortise_and_hare_template.rb
@@ -1,18 +1,22 @@
 # This is to serve as a template for using the Tortise and Hare algorithm 
 # This algorithm is used to detect cycles in a linked list
 
-def solution(head)
-  tortoise = head
-  hare = head
+def find_duplicate(nums)
+  tortoise = nums[0]
+  hare = nums[0]
 
-  while hare != nil && hare.next != nil
-    tortoise = tortoise.next
-    hare = hare.next.next
+  # Step 1: Finding the intersection point in the cycle
+  begin
+    tortoise = nums[tortoise]
+    hare = nums[nums[hare]]
+  end while tortoise != hare
 
-    if tortoise == hare
-      return true # cycle detected
-    end
+  # Step 2: Find the entrance to the cycle
+  tortoise = nums[0]
+  while tortoise != hare
+    tortoise = nums[tortoise]
+    hare = nums[hare]
   end
 
-  false # no cycle detected
+  tortoise
 end
